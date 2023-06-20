5c7178a79b7c496ea435fd4fef543d1f93624cf0
severo <oliveira.severo@gmail.com>
2023-06-20 14:25:38 -0300

    feat: :rocket: implementa o botão comecar o jogo na home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="home-page">
       <div className="title">
         <h1>Página Home</h1>
+        <button onClick={ () => navigate('/trivia') }>
+          Começar o jogo
+        </button>
       </div>
     </div>
   );
