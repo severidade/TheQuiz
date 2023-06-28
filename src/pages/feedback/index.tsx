0123eb573cb09417fc6f2403d27f28c8477aae5f
severo <oliveira.severo@gmail.com>
2023-06-28 10:57:08 -0300

    feat: :rocket: Tentativa de colocar o salvamento no banco em um botao

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import cliente from '../../cliente';
 
 export default function Feedback() {
   const userName = useSelector((state: { userName: string }) => state.userName);
+  const userEmail = useSelector((state: { userEmail: string }) => state.userEmail);
   const correctAnswers = useSelector(
     (state: { correctAnswers: number }) => state.correctAnswers,
   );
@@ -11,32 +12,52 @@ export default function Feedback() {
     (state: { numberOfQuestions: number }) => state.numberOfQuestions,
   );
 
-  useEffect(() => {
-    const handleSaveResults = async () => {
-      try {
-        if (userName && correctAnswers !== null && numberOfQuestions !== null) {
-          // Define os dados que serão salvos
-          const result = {
-            _type: 'ranking',
-            userName,
-            correctAnswers,
-            numberOfQuestions,
-          };
+  // useEffect(() => {
+  //   const handleSaveResults = async () => {
+  //     try {
+  //       if (userName && correctAnswers !== null && numberOfQuestions !== null) {
+  //         // Define os dados que serão salvos
+  //         const result = {
+  //           _type: 'ranking',
+  //           userName,
+  //           correctAnswers,
+  //           numberOfQuestions,
+  //         };
 
-          // Envie os dados para o Sanity
-          await cliente.create(result);
+  //         // Envie os dados para o Sanity
+  //         await cliente.create(result);
 
-          console.log('Resultados salvos com sucesso!');
-        } else {
-          console.log('Resultados inválidos, não foram salvos no Sanity.');
-        }
-      } catch (error) {
-        console.error('Erro ao salvar os resultados:', error);
-      }
-    };
+  //         console.log('Resultados salvos com sucesso!');
+  //       } else {
+  //         console.log('Resultados inválidos, não foram salvos no Sanity.');
+  //       }
+  //     } catch (error) {
+  //       console.error('Erro ao salvar os resultados:', error);
+  //     }
+  //   };
 
-    handleSaveResults();
-  }, [userName, correctAnswers, numberOfQuestions]);
+  //   handleSaveResults();
+  // }, [userName, correctAnswers, numberOfQuestions]);
+
+  const handleSaveResults = async () => {
+    try {
+      // Define os dados que serão salvos
+      const result = {
+        _type: 'ranking',
+        userName,
+        userEmail,
+        correctAnswers,
+        numberOfQuestions,
+      };
+
+      // Envie os dados para o Sanity
+      await cliente.create(result);
+
+      console.log('Resultados salvos com sucesso!');
+    } catch (error) {
+      console.error('Erro ao salvar os resultados:', error);
+    }
+  };
 
   return (
     <div className="feedback-page">
@@ -56,6 +77,7 @@ export default function Feedback() {
           {' '}
           {numberOfQuestions}
         </p>
+        {/* <button onClick={ handleSaveResults }>Salvar Resultados</button> */}
       </div>
     </div>
   );
