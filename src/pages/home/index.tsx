b6b16050df98842752a1657959b602f7204deaa8
severo <oliveira.severo@gmail.com>
2023-07-07 17:34:37 -0300

    refactor: :recycle: pula a validacao no banco para gar novamente
    Modifica a query para verificação no banco

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -27,30 +27,36 @@ export default function Home() {
   const handlePlay: React.MouseEventHandler<HTMLButtonElement> = async (e) => {
     e.preventDefault();
 
-    const query = '*[_type == \'user\' && (name == $name || email == $email)]';
-    const existingUsers = await client.fetch(query, { name, email });
-
-    if (existingUsers.length > 0) {
+    console.log(gamePlayedAgain);
+    if (gamePlayedAgain) {
       navigate('/trivia');
-      console.log('cheguei aqui');
+      console.log(`passei aqui e estou ${gamePlayedAgain}`);
+      console.log('pulei a validacao');
+
       dispatch(playAgain(name, email, false));
     } else {
-      Swal.fire({
-        icon: 'error',
-        title: 'Usuário não existe',
-        text: 'usuário nao cadastrado na base.',
-      });
+      // const query = '*[_type == \'user\' && (name == $name || email == $email)]';
+      const query = '*[_type == \'user\' && name == $name && email == $email]';
+
+      const existingUsers = await client.fetch(query, { name, email });
+
+      console.log(`passei aqui e estou ${gamePlayedAgain}`);
+      console.log('passei na validacao validacao pois estava false');
+
+      if (existingUsers.length > 0) {
+        navigate('/trivia');
+        dispatch(playAgain(name, email, false));
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Usuário não existe',
+          text: 'usuário não cadastrado na base.',
+        });
+      }
     }
   };
 
   useEffect(() => {
-    // setIsValid(false);
-    // const emailValidation = email.match(/\S+@\S+\.\S+/);
-    // const nameValidation = name.length;
-    // const magicNumber = 4;
-    // if (emailValidation && nameValidation >= magicNumber) {
-    //   setIsValid(true);
-    // }
     const handleisValidity = () => {
       const isNameValid = validateName(name);
       const isEmailValid = validateEmail(email);
@@ -60,11 +66,13 @@ export default function Home() {
     };
 
     handleisValidity();
+    console.log(`passei no segundo e estou ${gamePlayedAgain}`);
   }, [name, email]);
 
   useEffect(() => {
     const isUserNameValid = userName !== '';
     const isUserEmailValid = userEmail !== '';
+    console.log(`passei no primeiro aqui e estou ${gamePlayedAgain}`);
 
     if (
       gamePlayedAgain
@@ -79,11 +87,6 @@ export default function Home() {
       setIsValid(true);
     }
   }, [gamePlayedAgain, userName, userEmail]);
-
-  // console.log(typeof gamePlayedAgain);
-  // console.log(gamePlayedAgain ? 'Verdadeiro' : 'Falso');
-  // console.log(name, email);
-  // console.log(userName, userEmail);
 
   return (
     <div className="home-page">
