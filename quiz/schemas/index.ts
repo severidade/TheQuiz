26d985a73798ce62f32e62744ef67b561a0ea198
severo <oliveira.severo@gmail.com>
2023-07-09 15:01:24 -0300

    feat: :rocket: Cria um novo escema com perguntas para crianças

--- quiz/schemas/index.ts
+++ quiz/schemas/index.ts
@@ -3,7 +3,8 @@ import category from './category'
 import post from './post'
 import author from './author'
 import quiz from './quiz'
+import quizUnderTen from './quizUnderTen'
 import user from './user'
 import ranking from './ranking'
 
-export const schemaTypes = [post, author, category, blockContent, quiz, user, ranking]
+export const schemaTypes = [post, author, category, blockContent, quiz, quizUnderTen, user, ranking]
