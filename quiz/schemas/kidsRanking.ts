71395cc299d587da79914e8d88a8ba90729b0b88
severo <oliveira.severo@gmail.com>
2023-07-12 17:24:27 -0300

    feat: :rocket: Cria duas tabelas de ranking no sanity.io
    uma para adultos e outra para crianças

---
+++ quiz/schemas/kidsRanking.ts
@@ -0,0 +1,31 @@
+export default {
+  name: 'kids_ranking',
+  title: 'Ranking Crianças',
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      title: 'User Name',
+      type: 'string',
+      description: 'Enter the user name',
+    },
+    {
+      name: 'userEmail',
+      title: 'User e-mail',
+      type: 'string',
+      description: 'Enter the user e-mail address',
+    },
+    {
+      name: 'correctAnswers',
+      title: 'Correct Answers',
+      type: 'number',
+      description: 'Enter the number of correct answers',
+    },
+    {
+      name: 'numberOfQuestions',
+      title: 'Number of Questions',
+      type: 'number',
+      description: 'Enter the total number of questions',
+    },
+  ],
+};
